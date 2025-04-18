c7a1ca8ee798735aaffe2bc1bffb47fdd655350a
vanessaborges2 <65421224+vanessaborges2@users.noreply.github.com>
2025-04-17 21:13:39 -0300

    Update categorias.php

--- projeto/categorias.php
+++ projeto/categorias.php
@@ -1,9 +1,49 @@
 <?php
   require_once("cabecalho.php");
+
+  function retornaCategorias(){
+    require("conexao.php");
+    try{
+      $sql = "SELECT * from categoria";
+      $stmt = $pdo->query($sql);
+      return $stmt->fetchAll();
+    } catch (Exception $e){
+      die("Erro ao consultar as categorias: ". $e->getMessage());
+    }
+  }
+
+  $categorias = retornaCategorias();
+
 ?>
 
-  <h1>Categorias</h1>
-
+<h2>Categorias</h2>
+<a href="#" class="btn btn-success mb-3">Novo Registro</a>
+<table class="table table-hover table-striped">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+          foreach($categorias as $c):
+        ?>
+            <tr>
+                <td><?= $c['id'] ?></td>
+                <td><?= $c['nome'] ?></td>
+                <td>
+                    <a href="#" class="btn btn-warning">Editar</a>
+                    <a href="#" class="btn btn-info">Consultar</a>
+                </td>
+            </tr>
+        <?php
+          endforeach;
+        ?>
+    </tbody>
+</table>
+                
 <?php
   require_once("rodape.php");
 ?>
